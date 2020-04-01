174a6f286020e471e8755bd28329f2ccfc3cd028
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-01 22:05:13 +0300

    Revert "Changed MainPage to AppShell"
    This reverts commit d50aa77013cc9d9257052bbbcddb013a27d89d8f.

--- Nomadic/App.xaml.cs
+++ Nomadic/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Nomadic
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
