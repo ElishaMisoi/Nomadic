bbdfc850b9ea213a8b444efeb65d5633303ee71c
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-01 21:59:54 +0300

    Added IStatusBar Iterface

---
+++ Nomadic/Interfaces/IStatusBar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nomadic.Interfaces
+{
+    public interface IStatusBar
+    {
+        void ChangeStatusBarColorToBlack();
+
+        void ChangeStatusBarColorToWhite();
+
+        void HideStatusBar();
+    }
+}
