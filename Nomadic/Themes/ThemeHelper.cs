396214cef9c821a3a5810d26e6380a1b7d6e4717
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-01 22:00:56 +0300

    Added ThemeHelper to Check System Theme and Change App Theme

---
+++ Nomadic/Themes/ThemeHelper.cs
@@ -0,0 +1,30 @@
+﻿using Nomadic.Interfaces;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Nomadic.Themes
+{
+    public static class ThemeHelper
+    {
+        public static void GetSystemRequestedTheme()
+        {
+            AppTheme appTheme = AppInfo.RequestedTheme;
+
+            if (appTheme == AppTheme.Dark)
+            {
+                Application.Current.Resources = new DarkTheme();
+                DependencyService.Get<IStatusBar>().ChangeStatusBarColorToBlack();
+            }
+            else if (appTheme == AppTheme.Light)
+            {
+                Application.Current.Resources = new LightTheme();
+                DependencyService.Get<IStatusBar>().ChangeStatusBarColorToWhite();
+            }
+            else
+            {
+                App.Current.Resources = new LightTheme();
+                DependencyService.Get<IStatusBar>().ChangeStatusBarColorToWhite();
+            }    
+        }    
+    }    
+} 
