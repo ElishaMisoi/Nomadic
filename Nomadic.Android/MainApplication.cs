ad6be7b63c00a8b72be5af2fe3f4638441586d56
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-01 21:57:58 +0300

    Initialized CrossCurrentActivity

---
+++ Nomadic.Android/MainApplication.cs
@@ -0,0 +1,64 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Plugin.CurrentActivity;
+using System;
+
+namespace Nomadic.Droid
+{
+#if DEBUG
+    [Application(Debuggable = true)]
+#else
+	[Application(Debuggable = false)]
+#endif
+    public class MainApplication : Application
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
+        {
+
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            CrossCurrentActivity.Current.Init(this);
+        }
+
+        public void OnActivityCreated(Activity activity, Bundle savedInstanceState)
+        {
+            CrossCurrentActivity.Current.Activity = activity;
+        }
+
+        public void OnActivityDestroyed(Activity activity)
+        {
+
+        }
+
+        public void OnActivityPaused(Activity activity)
+        {
+
+        }
+
+        public void OnActivityResumed(Activity activity)
+        {
+            CrossCurrentActivity.Current.Activity = activity;
+        }
+
+        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+        {
+
+        }
+
+        public void OnActivityStarted(Activity activity)
+        {
+            CrossCurrentActivity.Current.Activity = activity;
+        }
+
+        public void OnActivityStopped(Activity activity)
+        {
+
+        }
+    }
+}
