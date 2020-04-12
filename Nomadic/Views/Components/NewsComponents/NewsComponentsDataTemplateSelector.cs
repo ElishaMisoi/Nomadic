b6fa1f2679819e395372326a5c6d74fd732675b7
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-12 22:42:17 +0300

    Added NewsComponentsDataTemplateSelector for Choosing which ViewCell to use

---
+++ Nomadic/Views/Components/NewsComponents/NewsComponentsDataTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Nomadic.Views.Components.NewsComponents
+{
+    public class NewsComponentsDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate NewsView { get; set; }
+        public DataTemplate WideNewsView { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (((Models.Article)item).IsWideView)
+                return WideNewsView;
+            return NewsView;
+        }
+    }
+}
