3ed32670f53c6dd457f777749838389caf587008
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-22 02:25:35 +0300

    Added ClientID's

--- Nomadic/Constants.cs
+++ Nomadic/Constants.cs
@@ -6,6 +6,23 @@ namespace Nomadic
 {
     public static class Constants
     {
-        public static string NewsAPIKey = "place your NewsAPI key here";
+        public const string NewsAPIKey = "place your NewsAPI key here";
+
+        public static string AppName = "Nomadic"; // Name of your Google App
+
+        // OAuth
+        // For Google login, configure at https://console.developers.google.com/
+        public static string GoogleiOSClientId = "place your iOS Google client Id here"; // example 6546314619-e6g8606ombclld9i4okci42akjhp.apps.googleusercontent.com
+        public static string GoogleAndroidClientId = "place your Android Google client Id here"; // example 6546314619-s8ugecvkan63tvk2u3hpkc0u71lj.apps.googleusercontent.com
+
+        // These values do not need changing
+        public static string GoogleScope = "https://www.googleapis.com/auth/userinfo.email";
+        public static string GoogleAuthorizeUrl = "https://accounts.google.com/o/oauth2/auth";
+        public static string GoogleAccessTokenUrl = "https://www.googleapis.com/oauth2/v4/token";
+        public static string GoogleUserInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
+
+        // Set these to reversed iOS/Android client ids, with :/oauth2redirect appended
+        public static string GoogleiOSRedirectUri = "place your iOS Google redirect uri here"; // example com.googleusercontent.apps.6546314619-e6g8606ombclld9i4okci42akjhp:/oauth2redirect
+        public static string GoogleAndroidRedirectUri = "place your Android Google redirect uri here"; // example com.googleusercontent.apps.6546314619-s8ugecvkan63tvk2u3hpkc0u71lj:/oauth2redirect
     }
 }
