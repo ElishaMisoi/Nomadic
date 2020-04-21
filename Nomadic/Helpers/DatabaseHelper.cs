ac751cc6936d9fbd14e685c62275a8b6ba16abb7
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-22 02:25:59 +0300

    Created DatabaseHelper

---
+++ Nomadic/Helpers/DatabaseHelper.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Nomadic.Models;
+using Plugin.CloudFirestore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomadic.Helpers
+{
+    public static class DatabaseHelper
+    {
+        public static async Task<User> GetUser(string userId)
+        {
+            try
+            {
+                var snapshot = await CrossCloudFirestore.Current
+                                            .Instance
+                                            .GetDocument($"users/{userId}")
+                                            .GetDocumentAsync();
+
+                return snapshot.ToObject<User>();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+        public static async Task AddUser(User user)
+        {
+            try
+            {
+                await CrossCloudFirestore.Current
+                        .Instance
+                        .GetDocument($"users/{user.UserID}")
+                        .SetDataAsync(user);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
