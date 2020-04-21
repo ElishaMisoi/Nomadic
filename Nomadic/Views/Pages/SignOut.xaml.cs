959dc1729550ceb64ab453f560e7083f320ad1c0
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-22 02:29:05 +0300

    Created SignOut Page

---
+++ Nomadic/Views/Pages/SignOut.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nomadic.Views.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignOut : ContentPage
+    {
+        public SignOut()
+        {
+            InitializeComponent();
+            BindingContext = ViewModels.SettingsViewModel.Instance;
+        }
+    }
+}
