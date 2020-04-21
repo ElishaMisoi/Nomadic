04547530ad031c1929ea9074652a2021270bce45
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-22 02:25:47 +0300

    Added AuthenticationState

---
+++ Nomadic/Helpers/Authentication/AuthenticationState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Auth;
+
+namespace Nomadic.Helpers.Authentication
+{
+	public class AuthenticationState
+	{
+		public static OAuth2Authenticator Authenticator;
+	}
+}
