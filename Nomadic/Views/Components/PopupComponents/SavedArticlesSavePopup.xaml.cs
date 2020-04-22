19aac419a8419e0392b2dddf974ea2b1c554e2cd
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-23 00:53:38 +0300

    Create SavedArticlesSavePopup.xaml.cs

---
+++ Nomadic/Views/Components/PopupComponents/SavedArticlesSavePopup.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nomadic.Views.Components.PopupComponents
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SavedArticlesSavePopup
+    {
+        public SavedArticlesSavePopup()
+        {
+            InitializeComponent();
+            BindingContext = ViewModels.SavedArticlesViewModels.Instance;
+        }
+    }
+}
