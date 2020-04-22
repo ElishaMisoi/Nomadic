4714f0323950ff93c85c2d89ce369043b2fcde07
Elisha Misoi <45200891+Elisha-Misoi@users.noreply.github.com>
2020-04-23 00:52:45 +0300

    Created EnumsHelper Class

---
+++ Nomadic/Helpers/EnumsHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nomadic.Helpers
+{
+    public static class EnumsHelper
+    {
+        public static string ConvertToString(this Enum eff)
+        {
+            return Enum.GetName(eff.GetType(), eff);
+        }
+
+        public static EnumType ConvertToEnum<EnumType>(this string enumValue)
+        {
+            return (EnumType)Enum.Parse(typeof(EnumType), enumValue);
+        }
+    }
+}
